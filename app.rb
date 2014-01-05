e1e41810f7a4c0f647635a80e728cb00da508158
Christian Fleschhut <weltrekordhalter@gmail.com>
2014-01-05 20:57:35 +0100

    display instagram user recent media items

--- app.rb
+++ app.rb
@@ -1,7 +1,13 @@
 require 'sinatra'
+require 'instagram'
+
+Instagram.configure do |config|
+  config.client_id = ENV['INSTAGRAM_CLIENT_ID']
+  config.access_token = ENV['INSTAGRAM_ACCESS_TOKEN']
+end
 
 get '/' do
-  @path = "index"
+  @recent_media = Instagram.user_recent_media(count: 25)
   erb :index
 end
 
